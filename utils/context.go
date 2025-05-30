e2ef43ebac9f7b1f8a9858c03705dc5e28bb2edf
Peyman <p.malek32@gmail.com>
2025-05-30 11:54:29 +0330

    add get claims with catch claims function

---
+++ utils/context.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mitchellh/mapstructure"
+	"github.com/ppeymann/vendors.git/auth"
+)
+
+const (
+	ContextUserKey = "CONTEXT_USER_KEY"
+	ContextHostKey = "HOST_KEY"
+)
+
+// ErrUserPrincipalsNotFound is returned when UserPrincipals are not found in the context.
+var ErrUserPrincipalsNotFound = errors.New("UserPrincipals not found in context")
+
+func CatchClaims(ctx *gin.Context, claims *auth.Claims) error {
+	user, ok := ctx.Get(ContextUserKey)
+	if !ok {
+		return errors.New("user not found in context")
+	}
+
+	err := mapstructure.Decode(user, claims)
+	if err != nil {
+		return errors.New("error parsing user object to claims")
+	}
+
+	return nil
+}
